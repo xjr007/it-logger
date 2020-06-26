7be5cfacab3ef0c0c2a513bba8d1f4f44da56303
Aaqiel <abaaqielbehardien@gmail.com>
2020-06-26 15:28:27 +0200

    add logs completed

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -30,6 +30,7 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 
 Logs.protoTypes = {
 	log: PropTypes.object.isRequired,
+	getLogs: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
